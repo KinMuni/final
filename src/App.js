1624483baf4e2dafd1fbbe7beda64ff3ac775fa8
kinMuni <kiny.muni@gmail.com>
2018-10-14 21:40:01 -0400

    -a

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+componentDidMount() {
+  this.loadMap()
+
+}
+  loadMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDK4Rzpk7QIeoxO5xb1CxNMceih2sop_NY&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+initMap = () => {
+  var map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -25.344,
+      lng: 131.036
+    },
+      zoom: 18  
+  })
+}
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main> 
+       <div id="map"></div>
+      </main> 
+    )
   }
 }
 
+{/*<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK4Rzpk7QIeoxO5xb1CxNMceih2sop_NY&callback=initMap">
+    </script>  */} 
+
+    function loadScript(url) {
+
+      var index = window.document.getElementsByTagName("script")[0]
+      var script = window.document.createElement("script")
+      script.src = url
+      script.async = true
+      script.defer = true
+      index.parentNode.insertBefore(script, index)
+
+    }
 export default App;
