cc0719ae73b3a8aa7b2a64b2aa42aa5d3a70b388
kinMuni <kiny.muni@gmail.com>
2018-10-25 17:12:48 -0500

    -a

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ class App extends Component {
   };
 
   getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
     const parameters = {
       client_id: "QANEM2RQ0MROGYQKMXITFC2AP5C21MY1I4JUWGXVJHIL4ZTK",
       client_secret: "WNDZEXOEG3QTHCJSS1SJPZP2BVYAQRHKW10DTYJBQEFHOW4H",
       query: "food",
-      near: "Manhattan",
-      v: "20181014"
-    };
+      near: "Sydney",
+      v: "20181020"
+    }
     axios
       .get(endPoint + new URLSearchParams(parameters))
       .then(response => {
@@ -38,105 +38,25 @@ class App extends Component {
 
       .catch(error => {
         console.log("ERROR!! " + error.response);
-      });
-  };
+      })
+  }
 
   initMap = () => {
 
     var map = new window.google.maps.Map(document.getElementById("map"), {
       center: {
-        lat:40.7831,
-        lng:73.9712
-      },
-      zoom: 15,
-      styles: [
-      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-      {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#263c3f'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#6b9a76'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#38414e'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#212a37'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9ca5b3'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#746855'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{color: '#1f2835'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#f3d19c'}]
-      },
-      {
-        featureType: 'transit',
-        elementType: 'geometry',
-        stylers: [{color: '#2f3948'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#d59563'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#17263c'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#515c6d'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#17263c'}]
-      }
-    ]
-    })
+        lat:-34.397,
+        lng:150.644
+              },
+      zoom: 11
+        })
+        //create an infow window
+        var infowindow = new window.google.maps.InfoWindow()
+        // Display Dynamic markers
 
      this.state.venues.map(myVenue => {
       var contentString = `${myVenue.venue.name}`
-    //create an infow window
-    var infowindow = new window.google.maps.InfoWindow({
-    content: contentString
-  });
+  // creeate a marker
       var marker= new window.google.maps.Marker({
       position: {
         lat: myVenue.venue.location.lat,
@@ -150,7 +70,7 @@ class App extends Component {
     marker.addListener('click', function() {
        // change the content
 
-       infowindow.setContent(contentString)
+  infowindow.setContent(contentString)
 
          //open an infowindow
     infowindow.open(map, marker)
@@ -164,6 +84,11 @@ class App extends Component {
     return (
      <main>
       <div id ="map">
+      <div className = "container">
+      <div className = "text-input">
+
+      </div>
+      </div>
       </div>
       </main >
     )
